cd93d712b518e0ca3bb91abdefb5df49a89cbbd2
Yuanmxc <1657615110@qq.com>
2023-05-13 22:17:53 +0800

    更新

--- loadManager/loadmanager.go
+++ loadManager/loadmanager.go
@@ -1,4 +1,5 @@
 package loadmanager
 
 type LoadManager interface {
+	
 }
--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	_ "MxcMQ/config"
+	"MxcMQ/logger"
+	_ "MxcMQ/persist"
+	_ "MxcMQ/registrationCenter"
 	"MxcMQ/server"
 )
 
 func main() {
 	server := server.NewServerFromConfig()
+	if err := server.Online(); err != nil {
+		logger.Errorf("Online failed: %v", err)
+	}
+
 	server.RunWithGrpc()
 }
