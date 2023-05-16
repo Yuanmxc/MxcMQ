a264258544b50d72902d9b1df8d45b11c28d6521
Yuanmxc <1657615110@qq.com>
2023-05-16 22:49:02 +0800

    添加客户端部分

--- main.go
+++ main.go
@@ -1,18 +1,66 @@
 package main
 
 import (
-	_ "MxcMQ/config"
+	"MxcMQ/config"
 	"MxcMQ/logger"
-	_ "MxcMQ/persist"
-	_ "MxcMQ/registrationCenter"
+	"MxcMQ/persist"
+	rc "MxcMQ/registrationCenter"
 	"MxcMQ/server"
+	"os"
+	"time"
+
+	"github.com/urfave/cli/v2"
+)
+
+var (
+	cmdStart = cli.Command{
+		Name:  "start",
+		Usage: "start server. For example: ./MxcMQ start -c config/config.yaml",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "c",
+				Usage: "Load configuration from `FILE`",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			config.GetConfig(c.String("c"))
+			persist.PersistInit()
+			rc.RcInit()
+			
+			server := server.NewServerFromConfig()
+			if err := server.Online(); err != nil {
+				logger.Errorf("Online failed: %v", err)
+			}
+
+			server.RunWithGrpc()
+			return nil
+		},
+	}
 )
 
 func main() {
-	server := server.NewServerFromConfig()
-	if err := server.Online(); err != nil {
-		logger.Errorf("Online failed: %v", err)
+	app := newapp(&cmdStart)
+	_ = app.Run(os.Args)
+}
+
+func newapp(startCmd *cli.Command) *cli.App {
+	app := cli.NewApp()
+	app.Name = "MxcMQ"
+	app.Compiled = time.Now()
+	app.Usage = "A High Performance MxcMQ Server written in Go."
+	app.Flags = cmdStart.Flags
+
+	app.Commands = []*cli.Command{
+		&cmdStart,
 	}
 
-	server.RunWithGrpc()
+	app.Action = func(c *cli.Context) error {
+		if c.NumFlags() == 0 {
+			return cli.ShowAppHelp(c)
+		}
+
+		return startCmd.Action(c)
+	}
+
+	return app
 }
